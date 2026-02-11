56bab52e2ab872c9d08ad3a5f2589dfa8d1fdc05
伊藤 駿 <rendya2501@gmail.com>
2026-02-11 22:55:10 +0900

    監査ログをAuditLogResponseに変換する拡張メソッドにドキュメントコメントを追加しました。

--- src/Web.Api/Contracts/AuditLogs/Responses/AuditLogMappingExtensions.cs
+++ src/Web.Api/Contracts/AuditLogs/Responses/AuditLogMappingExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace Web.Api.Contracts.AuditLogs.Responses;
 
+/// <summary>
+/// <see cref="AuditLog"/> ドメインモデルを <see cref="AuditLogResponse"/> に変換する拡張メソッド
+/// </summary>
 public static class AuditLogMappingExtensions
 {
+    /// <summary>
+    /// <see cref="AuditLog"/> を <see cref="AuditLogResponse"/> に変換します
+    /// </summary>
+    /// <param name="log">変換対象の監査ログ</param>
+    /// <returns>監査ログのレスポンスDTO</returns>
     public static AuditLogResponse ToResponse(this AuditLog log) =>
         new(
             log.Id,
