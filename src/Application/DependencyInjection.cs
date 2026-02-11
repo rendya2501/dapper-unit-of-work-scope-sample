54bbd3dc824791cfa911a9c1f8225e5ce3469e2c
伊藤 駿 <rendya2501@gmail.com>
2026-02-11 22:54:29 +0900

    Applicationレイヤーの依存性注入設定に関するドキュメントを追加し、Scopedライフタイムで登録されるサービスの詳細を明記しました。

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -3,8 +3,24 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
 
+/// <summary>
+/// Application レイヤーの依存性注入設定
+/// </summary>
 public static class DependencyInjection
 {
+    /// <summary>
+    /// Application レイヤーの依存性を登録します。
+    /// </summary>
+    /// <remarks>
+    /// <para>以下のサービスを Scoped ライフタイムで登録します。</para>
+    /// <list type="bullet">
+    /// <item><see cref="AuditLogService"/> - 監査ログのビジネスロジック</item>
+    /// <item><see cref="InventoryService"/> - 在庫管理のビジネスロジック</item>
+    /// <item><see cref="OrderService"/> - 注文管理のビジネスロジック</item>
+    /// </list>
+    /// </remarks>
+    /// <param name="services">サービスコレクション</param>
+    /// <returns>メソッドチェーン用の <see cref="IServiceCollection"/></returns>
     public static IServiceCollection AddApplication(
         this IServiceCollection services)
     {
